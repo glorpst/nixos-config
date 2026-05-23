905e814a5c9fd08a196bf9a1208dd1b189b1dc76
glorpst <128099126+glorpst@users.noreply.github.com>
2026-05-23 04:21:32 -0500

    added laptop configuration

--- flake.nix
+++ flake.nix
@@ -9,6 +9,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    disko = {
+      url = "github:nix-community/disko";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     nvf = {
       url = "github:NotAShelf/nvf";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -17,14 +22,29 @@
 
   outputs = { self, nixpkgs, nvf, ... } @ inputs:
     let
-      sysUser = "glorpst"; # change per system user
+      sysUser = "peebs"; # change per system user
     in {
-    nixosConfigurations.nixos-vm = nixpkgs.lib.nixosSystem { # change nixosConfig.x to host name
-      specialArgs = { inherit inputs sysUser; };
-      modules = [
-        ./hosts/nixos-vm/configuration.nix
-        inputs.home-manager.nixosModules.default
-      ];
+    nixosConfigurations = {
+        # Host: NixOS VM
+        nixos-vm = nixpkgs.lib.nixosSystem {
+          system = "x86_64-linux";
+          specialArgs = { inherit inputs sysUser; };
+          modules = [
+            ./hosts/nixos-vm/configuration.nix # change file path to match hosts/ dir name
+            inputs.home-manager.nixosModules.default
+          ];
+        };
+
+        # Host: Laptop
+        laptop = nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs sysUser; };
+          modules = [
+            ./hosts/laptop/configuration.nix # change file path to match hosts/ dir name
+            ./hosts/laptop/disko-config.nix
+            inputs.home-manager.nixosModules.default
+            inputs.disko.nixosModules.default
+          ];
+        };
     };
   };
 }
