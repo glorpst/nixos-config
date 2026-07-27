cceb1a7b6e2082fcf26830c9fd7459fa7ea6362e
glorpst <128099126+glorpst@users.noreply.github.com>
2026-07-26 23:55:05 -0500

    cli/ sub-directory

--- modules/hosts/bmo/configuration.nix
+++ modules/hosts/bmo/configuration.nix
@@ -6,7 +6,6 @@
       self.nixosModules.laptopHardware
       self.nixosModules.laptopDisko
       self.nixosModules.niri
-      self.nixosModules.noctalia
       self.nixosModules.gaming
       inputs.home-manager.nixosModules.default
       inputs.disko.nixosModules.default
